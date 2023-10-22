42f43eb5d5c5437884202a655165c9c5c30c70f4
Nicolas-Espel <101594373+Nicolas-Espel@users.noreply.github.com>
2023-10-22 15:13:41 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    var lineOne = "Now, this is a story all about how";
+    var lineTwo = "My life got flipped-turned upside down";
+    var lineThree = "And I'd like to take a minute";
+    var lineFour = "Just sit right there";
+    Console.WriteLine (lineOne);
+    Console.WriteLine (lineTwo);
+    Console.WriteLine (lineThree);
+    Console.WriteLine (lineFour);
+  }
+}
